929d4d75ea441f53061140cf9ece792b953cefba
Kuan-Hao Chao <kuanhao.chao@gmail.com>
2023-10-09 09:20:27 -0400

    Update extra copy finding module

--- lifton/extra_copy.py
+++ lifton/extra_copy.py
@@ -41,7 +41,7 @@ def find_extra_copy(m_feature_db, tree_dict, aa_id_2_m_id_dict, gene_info_dict, 
                 #######################################
                 trans_attrs = trans_info_dict[extra_cp_trans_id]
                 # print("trans_attrs: ", trans_attrs)
-                new_extra_cp_trans_id = Lifton_gene_ecp.create_new_transcript(False, extra_cp_gene_id, extra_cp_trans_id, trans_entry_base, chromosome, mtrans.start, mtrans.end, trans_attrs, gene_copy_num_dict)
+                new_extra_cp_trans_id = Lifton_gene_ecp.create_new_transcript(False, extra_cp_gene_id, extra_cp_trans_id, trans_entry_base, chromosome, mtrans.start, mtrans.end, trans_attrs, gene_copy_num_dict, trans_copy_num_dict)
 
                 #######################################
                 # Step 5.3: Create the exon entry
@@ -59,38 +59,35 @@ def find_extra_copy(m_feature_db, tree_dict, aa_id_2_m_id_dict, gene_info_dict, 
                     Lifton_gene_ecp.add_cds(new_extra_cp_trans_id, cds_copy)
                 Lifton_gene_ecp.write_entry(fw)
 
-            # else:
-            #     # Reason it's missing => the mRNA does not belong to gene (vdj segments)
-            #     NEW_LOCUS_MINIPROT_COUNT += 1
-            #     #######################################
-            #     # Step 5.1: Create the gene entry
-            #     #######################################
-            #     extra_cp_gene_id = "gene-LiftOn"
-            #     Lifton_gene_ecp = lifton_class.Lifton_GENE(gene_entry_base)
-            #     gene_attrs = lifton_class.Lifton_GENE_info({}, extra_cp_gene_id)
-            #     new_extra_cp_gene_id = Lifton_gene_ecp.update_gene_info(extra_cp_gene_id, chromosome, mtrans.start, mtrans.end, gene_attrs, gene_copy_num_dict)
+            else:
+                # Reason it's missing => the mRNA does not belong to gene (vdj segments)
+                NEW_LOCUS_MINIPROT_COUNT += 1
+                #######################################
+                # Step 5.1: Create the gene entry
+                #######################################
+                extra_cp_gene_id = "gene-LiftOn"
+                Lifton_gene_ecp = lifton_class.Lifton_GENE(gene_entry_base)
+                gene_attrs = lifton_class.Lifton_GENE_info({}, extra_cp_gene_id)
+                new_extra_cp_gene_id = Lifton_gene_ecp.update_gene_info(extra_cp_gene_id, chromosome, mtrans.start, mtrans.end, gene_attrs, gene_copy_num_dict)
 
-            #     # print("new_extra_cp_gene_id: ", new_extra_cp_gene_id)
+                #######################################
+                # Step 5.2: Create the transcript entry
+                #######################################
+                trans_attrs = lifton_class.Lifton_TRANS_info({}, extra_cp_trans_id, extra_cp_gene_id)
+                new_extra_cp_trans_id = Lifton_gene_ecp.create_new_transcript(True, extra_cp_gene_id, extra_cp_trans_id, trans_entry_base, chromosome, mtrans.start, mtrans.end, trans_attrs, gene_copy_num_dict, trans_copy_num_dict)
 
-
-            #     #######################################
-            #     # Step 5.2: Create the transcript entry
-            #     #######################################
-            #     trans_attrs = lifton_class.Lifton_TRANS_info({}, extra_cp_trans_id, extra_cp_gene_id)
-            #     new_extra_cp_trans_id = Lifton_gene_ecp.create_new_transcript(True, extra_cp_gene_id, extra_cp_trans_id, trans_entry_base, chromosome, mtrans.start, mtrans.end, trans_attrs, trans_copy_num_dict)
-
-            #     #######################################
-            #     # Step 5.3: Create the exon entry
-            #     #######################################
-            #     #######################################
-            #     # Step 5.4: Create the CDS entry
-            #     #######################################
-            #     cdss = m_feature_db.children(mtrans, featuretype='CDS')  # Replace 'exon' with the desired child feature type
-            #     # print("cdss len: ", len(cdss))
-            #     for cds in list(cdss):
-            #         Lifton_gene_ecp.add_exon(extra_cp_trans_id, cds)
-            #         cds_copy = copy.deepcopy(cds)
-            #         Lifton_gene_ecp.add_cds(extra_cp_trans_id, cds_copy)
-            #     Lifton_gene_ecp.write_entry(fw)
+                #######################################
+                # Step 5.3: Create the exon entry
+                #######################################
+                #######################################
+                # Step 5.4: Create the CDS entry
+                #######################################
+                cdss = m_feature_db.children(mtrans, featuretype='CDS')  # Replace 'exon' with the desired child feature type
+                # print("cdss len: ", len(cdss))
+                for cds in list(cdss):
+                    Lifton_gene_ecp.add_exon(extra_cp_trans_id, cds)
+                    cds_copy = copy.deepcopy(cds)
+                    Lifton_gene_ecp.add_cds(extra_cp_trans_id, cds_copy)
+                Lifton_gene_ecp.write_entry(fw)
 
     return EXTRA_COPY_MINIPROT_COUNT, NEW_LOCUS_MINIPROT_COUNT
