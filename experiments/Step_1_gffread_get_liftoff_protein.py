b39890a840f416bcc63bc670ba18ece68bfe3800
Kuan-Hao Chao <kuanhao.chao@gmail.com>
2023-11-07 18:04:45 -0500

    Add experiment scripts

---
+++ experiments/Step_1_gffread_get_liftoff_protein.py
@@ -0,0 +1,47 @@
+from gffutils import FeatureDB
+import pybedtools
+import os
+
+import sys
+from gffutils import FeatureDB
+from Bio import SeqIO
+import subprocess
+
+TARGET = sys.argv[1]
+
+if TARGET == "CHM13_MANE" or TARGET == "CHM13_RefSeq" or TARGET == "GRCh38_RefSeq" or TARGET == "Han1" or TARGET == "Ash1" or TARGET == "PR1" or TARGET == "Mus_musculus_MANE":
+    print("Running with ", TARGET)
+    genome = ""
+    if TARGET == "GRCh38_RefSeq":
+        genome = "/ccb/salz3/kh.chao/ref_genome/homo_sapiens/NCBI_Refseq_chr_fixed/GCF_000001405.40_GRCh38.p14_genomic.fna"
+
+    elif TARGET == "CHM13_MANE": 
+        genome = "/ccb/salz3/kh.chao/ref_genome/homo_sapiens/T2T-CHM13/chm13v2.0.fa"
+
+    elif TARGET == "CHM13_RefSeq":
+        genome = "/ccb/salz3/kh.chao/ref_genome/homo_sapiens/T2T-CHM13/chm13v2.0.fa"
+
+    elif TARGET == "Han1":
+        genome = "/ccb/salz3/kh.chao/PR_liftoff_protein_search/data/Han1/v1.0/Assembly/Han1_v1.2.fasta"
+
+    elif TARGET == "Ash1":
+        genome = "/ccb/salz3/kh.chao/PR_liftoff_protein_search/data/Ash1/v2.2/Assembly/Ash1_v2.2.fa"
+
+    elif TARGET == "PR1":
+        genome = "/ccb/salz3/kh.chao/PR_liftoff_protein_search/data/PR1/v3.0/Assembly/PR1.fa"
+
+    elif TARGET == "Mus_musculus_MANE":
+        genome = "/ccb/salz3/kh.chao/ref_genome/mus_musculus/NCBI_Refseq/GCF_000001635.27_GRCm39_genomic.fna"
+else:
+    sys.exit(-1)
+
+refernece_gff3 = f"/ccb/salz2/kh.chao/Lifton/results/{TARGET}/{TARGET}_lifton.gff3" 
+output_protein_fa = f"/ccb/salz2/kh.chao/Lifton/results/{TARGET}/{TARGET}_lifton_protein.fa"
+
+
+print("refernece_gff3: ", refernece_gff3)
+print("output_protein_fa: ", output_protein_fa)
+
+command = ['gffread', '-S', '-y', output_protein_fa, '-g', genome, refernece_gff3]
+
+gffread_extract = subprocess.call(command)
---
+++ experiments/miniprot_lifton_comparison/Step_5_plot_frequency_plot.py
@@ -0,0 +1,80 @@
+import parasail
+import os
+import pybedtools
+
+import sys
+from gffutils import FeatureDB
+from Bio import SeqIO
+import subprocess
+import matplotlib.pyplot as plt
+
+def get_id_fraction(reference, target):
+    matches = 0
+    for i, letter in enumerate(reference):
+        if letter == target[i]:
+            matches += 1
+    return matches, max(len(reference), len(target))
+
+TARGET = sys.argv[1]
+
+protein_fa = ""
+if TARGET == "CHM13_MANE" or TARGET == "CHM13_RefSeq" or TARGET == "GRCh38_RefSeq" or TARGET == "Han1" or TARGET == "Ash1" or TARGET == "PR1" or TARGET == "Mus_musculus_MANE":
+    print("Running with ", TARGET)
+    genome = ""
+    if TARGET == "GRCh38_RefSeq":
+        genome = "/ccb/salz3/kh.chao/ref_genome/homo_sapiens/NCBI_Refseq_chr_fixed/GCF_000001405.40_GRCh38.p14_genomic.fna"
+
+    elif TARGET == "CHM13_MANE": 
+        genome = "/ccb/salz3/kh.chao/ref_genome/homo_sapiens/T2T-CHM13/chm13v2.0.fa"
+
+    elif TARGET == "CHM13_RefSeq":
+        genome = "/ccb/salz3/kh.chao/ref_genome/homo_sapiens/T2T-CHM13/chm13v2.0.fa"
+
+    elif TARGET == "Han1":
+        genome = "/ccb/salz3/kh.chao/PR_liftoff_protein_search/data/Han1/v1.0/Assembly/Han1_v1.2.fasta"
+
+    elif TARGET == "Ash1":
+        genome = "/ccb/salz3/kh.chao/PR_liftoff_protein_search/data/Ash1/v2.2/Assembly/Ash1_v2.2.fa"
+
+    elif TARGET == "PR1":
+        genome = "/ccb/salz3/kh.chao/PR_liftoff_protein_search/data/PR1/v3.0/Assembly/PR1.fa"
+
+    elif TARGET == "Mus_musculus_MANE":
+        genome = "/ccb/salz3/kh.chao/ref_genome/mus_musculus/NCBI_Refseq/GCF_000001635.27_GRCm39_genomic.fna"
+else:
+    sys.exit(-1)
+
+
+
+outdir_root = f"/ccb/salz2/kh.chao/Lifton/results/{TARGET}/miniprot_lifton_cmp/"
+
+
+os.makedirs(outdir_root + "images/", exist_ok=True)
+
+fname = outdir_root + "identities.txt"
+
+
+
+targets = ["lifton", "miniprot"]
+for idx in range(len(targets)):
+
+    nums = []
+    with open(fname, "r") as fr:
+        lines = fr.read().splitlines()
+        for line in lines:
+            eles = line.split("\t")
+            nums.append(float(eles[idx+1]))
+            # print(eles[1])
+
+    figure_path = outdir_root + "images/"+targets[idx]+"_frequency.png"
+
+    plt.hist(nums, bins=100)
+    plt.gca().set(title='Score frequency histogram', ylabel='Frequency')
+
+    # plt.xlabel('lifton score')
+    # plt.ylabel('miniprot score')
+    # plt.title('Comparing lifton vs miniprot protein searching scores')
+    plt.tight_layout()
+    plt.savefig(figure_path, dpi=300)
+    plt.close()
+    plt.clf()
