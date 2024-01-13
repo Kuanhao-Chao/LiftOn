05f69c8d8d3b1918bba3cb2de476b2acf892a8c0
am12 <pebblealan@gmail.com>
2024-01-12 20:05:51 -0500

    extraction works in principle

--- comparison/mouse_to_rat_results/target_extraction.py
+++ comparison/mouse_to_rat_results/target_extraction.py
@@ -12,25 +12,18 @@ def read_targets(path):
         return target_ids
 
 def extract_targets(db1, db2, db3, targets):
-    # Create a new GFF file for the extracted transcripts
-    output_gff_file = 'combined_extracted_targets.gff'
-    with open(output_gff_file, 'w') as output_gff:
-        # Iterate through target IDs
-        for target_id in targets:
-            # Search for the feature with the matching ID in the combined FeatureDB
-            feature = combined_db[target_id]
-            if feature:
-                # Write the GFF line to the new GFF file
-                output_gff.write(str(feature) + '\n')
+   
+    for target in targets:
+        print(f'Extracting for target {target}...')
 
-                # Print relevant information about the target
-                print(f"Target ID: {target_id}")
-                print(f"Chromosome: {feature.seqid}")
-                print(f"Start: {feature.start}")
-                print(f"End: {feature.end}")
-                print(f"Strand: {feature.strand}")
-                print(f"Attributes: {feature.attributes}")
-                print("-" * 20)
+        feature1 = db1[target]
+        print(str(feature1) + '\n')
+        feature2 = db2[target]
+        print(str(feature2) + '\n')
+        feature3 = db3[target]
+        print(str(feature3) + '\n')
+        
+
 
 def main():
 
@@ -38,10 +31,8 @@ def main():
     miniprot_db = gffutils.FeatureDB('./lifton_output/miniprot_target/miniprot_target.db', keep_order=True)
     lifton_db = gffutils.FeatureDB('./lifton_output/lifton/lifton.db', keep_order=True)
     targets = read_targets('./extracted_gff/targets.txt')
-    
-    print(str(miniprot_db['rna-NM_001305875.1']))
 
-    #extract_targets(liftoff_db, miniprot_db, lifton_db, targets)
+    extract_targets(liftoff_db, miniprot_db, lifton_db, targets)
 
 
 if __name__ == '__main__': 
