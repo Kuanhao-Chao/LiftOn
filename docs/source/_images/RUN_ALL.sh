510c863668a5b8a37661ced1344a3369a4a45bb7
Kuan-Hao Chao <kuanhao.chao@gmail.com>
2024-03-04 14:28:00 -0500

    Update docs figures

--- docs/source/_images/RUN_ALL.sh
+++ docs/source/_images/RUN_ALL.sh
@@ -5,14 +5,14 @@ declare -a arr=("human_refseq" "human_mane" "human_chess" "rice" "arabadop" "bee
 for i in "${arr[@]}"
 do 
     echo "running $i"
-    # # Cp all figures
-    # echo cp -r /ccb/salz2/kh.chao/LiftOn/results/$i/lifton_output/visualization/* $i/
-    # cp -r /ccb/salz2/kh.chao/LiftOn/results/$i/lifton_output/visualization/* $i/
-    # rm -rf $i/Liftoff/
-    # rm -rf $i/miniprot/
-    # rm -rf $i/liftoff_frequency.png
-    # rm -rf $i/lifton_frequency.png
-    # rm -rf $i/miniprot_frequency.png
+    # Cp all figures
+    echo cp -r /ccb/salz2/kh.chao/LiftOn/results/$i/lifton_output/visualization/* $i/
+    cp -r /ccb/salz2/kh.chao/LiftOn/results/$i/lifton_output/visualization/* $i/
+    rm -rf $i/Liftoff/
+    rm -rf $i/miniprot/
+    rm -rf $i/liftoff_frequency.png
+    rm -rf $i/lifton_frequency.png
+    rm -rf $i/miniprot_frequency.png
 
     # # Cp gene order plot
     # echo cp -r /ccb/salz2/kh.chao/LiftOn/results/$i/liftofftools_output/gene_order_plot.pdf $i/
@@ -21,6 +21,6 @@ do
 
     # rm -rf $i/gene_order_plot.pdf
 
-    convert -density 250 $i/circos_plot.pdf $i/circos_plot.png
+    # convert -density 250 $i/circos_plot.pdf $i/circos_plot.png
 
 done
